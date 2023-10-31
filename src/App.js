8080e866d3d0f32b156bf6344d0c495c78755695
Deepak Shivaiah Prakash <deepak.shivaiah@bendigoadelaide.com.au>
2023-10-31 11:10:18 +1100

    change password calls working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,142 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import {initiateTx, validateOtp, changePassword, data} from './calls';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
     </div>
+  );
+}
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+
+
+
+
+
+function App() {
+  const [value, setValue] = React.useState(0);
+  const [flowId, setFlowId] = React.useState(0);
+  const [otpValue, setOtpValue] = React.useState()
+  const [password, setPassword] = React.useState('!Confirm1234u')
+  const [response, setResponse] = React.useState({})
+
+  const validateOtpPayload = (flowId, otp) => ({
+    "flowId": flowId,
+    "otp": otp
+})
+
+
+const changePasswordPayload = (flowId, password) => ({
+  "flowId": flowId,
+  "password": password
+})
+
+
+
+  const handleChange = async (event, newValue) => {
+    setResponse({})
+    setValue(newValue);
+  };
+
+  const handleOtp = (event) => {
+    setOtpValue(event.target.value);
+  }
+
+  const handleInitiateTx = async () => {
+    const response = await initiateTx()
+    setResponse(response.data)
+    setFlowId(response.data.flowId)
+  }
+
+  const handleValidateOtp = async () => {
+    const response = await validateOtp(validateOtpPayload(flowId, otpValue))
+    setResponse(response)
+  }
+
+  const handlePassword = (event) => {
+    setPassword(event.target.value);
+  }
+
+  const handleChangePassword = async () => {
+    const response = await changePassword(changePasswordPayload(flowId, password))
+    setResponse(response)
+  }
+
+
+  return (
+    <Box
+      sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: 224 }}
+    >
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={value}
+        onChange={handleChange}
+        aria-label="Vertical tabs example"
+        sx={{ borderRight: 1, borderColor: 'divider' }}
+      >
+        <Tab label="Step One: Initiate TX" {...a11yProps(0)} />
+        <Tab label="Step Two: Validate OTP" {...a11yProps(1)} />
+        <Tab label="Step Three: Change Password" {...a11yProps(2)} />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        <strong>Payload</strong>
+        <pre className="prettyprint lang-html" style={{ background: 'grey', color: 'white', padding: '20px'}}>{JSON.stringify(data, null, 4)}</pre>
+        <button type="submit" onClick={handleInitiateTx}>Initiate tx</button>
+        <br/><br/>
+        <strong>Response</strong>
+        <pre className="prettyprint lang-html" style={{ background: 'grey', color: 'white', padding: '20px'}}>{JSON.stringify(response, null, 4)}</pre>
+      </TabPanel>
+
+
+      <TabPanel value={value} index={1}>
+        Enter OTP: <input type="tel" value={otpValue} onChange={handleOtp}/>
+        <br/><br/><br/>
+        <strong>Payload</strong>
+        <pre className="prettyprint lang-html" style={{ background: 'grey', color: 'white', padding: '20px'}}>{JSON.stringify(validateOtpPayload(flowId, otpValue), null, 4)}</pre>
+        <button type="submit" onClick={handleValidateOtp} value={otpValue}>Validate OTP</button>
+        <br/><br/>
+        <strong>Response</strong>
+        <pre className="prettyprint lang-html" style={{ background: 'grey', color: 'white', padding: '20px'}}>{JSON.stringify(response, null, 4)}</pre>
+      </TabPanel>
+
+
+      <TabPanel value={value} index={2}>
+        Enter Password: <input type="text" value={password} onChange={handlePassword}/>
+        <br/><br/><br/>
+        <strong>Payload</strong>
+        <pre className="prettyprint lang-html" style={{ background: 'grey', color: 'white', padding: '20px'}}>{JSON.stringify(changePasswordPayload(flowId, password), null, 4)}</pre>
+        <button type="submit" onClick={handleChangePassword} value={otpValue}>Confirm password</button>
+        <br/><br/>
+        <strong>Response</strong>
+        <pre className="prettyprint lang-html" style={{ background: 'grey', color: 'white', padding: '20px'}}>{JSON.stringify(response, null, 4)}</pre>
+      </TabPanel>
+    </Box>
   );
 }
 
---
+++ src/calls.js
@@ -0,0 +1,101 @@
+import axios from "axios";
+
+export const data = { 
+    "msg": "This is transaction approval",
+    "subject": "This is subject",
+    "txFlow": "changePassword"
+};
+
+var config = (url, payload) => ({
+  method: "post",
+  url: `http://localhost:5080/${url}`,
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    "x-brand": "ben",
+    "x-channel": "mob",
+    "x-access-id": "3067183801",
+    "x-connect-tran-id": "94a2fda3-64d4-4613-b3b7-f97fa79f40e8",
+    "x-digital-uuid": "digital01",
+    "x-host": "apiTest",
+    "x-session-id": "session01",
+    "x-user-uuid": "user01",
+    "x-ibm-client-id": "",
+    "x-ibm-client-secret": "",
+    "x-global-transaction-id": "94a2fda3-64d4-4613-b3b7-f97fa79f40e8",
+    "x-member-id": "T01",
+    "x-tx-channel": "mob",
+  },
+  data: payload ? payload : data,
+});
+
+// const headers = [
+//     'Content-Type',
+//     'Accept',
+//     'x-brand',
+//     'x-channel',
+//     'x-access-id',
+//     'x-connect-tran-id',
+//     'x-digital-uuid',
+//     'x-host',
+//     'x-session-id',
+//     'x-user-uuid',
+//     'x-ibm-client-id',
+//     'x-ibm-client-secret',
+//     'x-global-transaction-id',
+//     'x-member-id',
+//     'x-tx-channel'
+//   ];
+
+// Enables verbose validation errors.
+// inside default.js
+// routes: {
+//     validate: {
+//       failAction: (_: any, __: any, error: any): any => error
+//     },
+//     cors: {
+//       origin: ['http://localhost:3001'],
+//       headers: headers
+//     }
+//   }
+
+export async function initiateTx() {
+  const response = await axios(config('v1/eid/identity/tx'))
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+      return response
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+    return response
+}
+
+export async function validateOtp(payload) {
+    const response = await axios(config('v1/eid/identity/tx/validate', payload))
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+      return response.data
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+    return response
+}
+
+export async function changePassword(payload) {
+    const response = await axios(config('v1/eid/identity/change-password', payload))
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+      return response.data
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+    return response
+}
+
+
